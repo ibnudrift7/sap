2f750d9c77a405f26800c3fef0480d222915d9c0
Pandu Bagoes Pradana <pandubaguspr@gmail.com>
2019-09-02 15:09:16 +0700

    cek dan kurang slidder

--- protected/views/home/index.php
+++ protected/views/home/index.php
@@ -7,7 +7,9 @@
                     <div class="inner">
                         <h4>Get to know SAP</h4>
                         <p>Being in the plastic business since 1975 has bring us so many things other couldn’t achieve.</p>
-                        <p class="readmore">READ MORE</p>
+                    </div>
+                    <div class="readmore">
+                        <p>READ MORE</p>
                         <img class="arrow" src="<?php echo $this->assetBaseurl; ?>right-arrow.png" alt="">
                     </div>
                 </div>
@@ -19,6 +21,10 @@
                         <h4>Our Markets</h4>
                         <p>With more than 4 decades of operations, our business has vastly expand to cover a bigger market.</p>
                     </div>
+                    <div class="readmore">
+                        <p>READ MORE</p>
+                        <img class="arrow" src="<?php echo $this->assetBaseurl; ?>right-arrow.png" alt="">
+                    </div>
                 </div>
             </div>
             <div class="col-md-15">
@@ -26,7 +32,11 @@
                     <div class="image"><img src="<?php echo $this->assetBaseurl; ?>homesec-3.jpg" alt=""></div>
                     <div class="inner">
                         <h4>SAP Products</h4>
-                        <p>We develop a variety of plastic products from food grade packagings, housewares, nonwovens and now UPVC roofings. </p>
+                        <p>We develop a variety of plastic products from food grade packagings, housewares, nonwovens and now UPVC roofings.</p>
+                    </div>
+                    <div class="readmore-2-kanan">
+                        <p>READ MORE</p>
+                        <img class="arrow" src="<?php echo $this->assetBaseurl; ?>right-arrow.png" alt="">
                     </div>
                 </div>
             </div>
@@ -36,6 +46,10 @@
                     <div class="inner">
                         <h4>SAP Solutions</h4>
                         <p>There’s so much capabilities and service solutions that we possess, in order to help you achieve your needs.</p>
+                    </div>
+                    <div class="readmore-2-kanan">
+                        <p>READ MORE</p>
+                        <img class="arrow" src="<?php echo $this->assetBaseurl; ?>right-arrow.png" alt="">
                     </div>
                 </div>
             </div>
@@ -48,11 +62,16 @@
         <div class="row">
             <div class="col-md-60">
                 <div class="box-content">
+                    <div class="image"><img src="<?php echo $this->assetBaseurl; ?>homesec2.jpg" alt=""></div>
                     <div class="title">
-                        <h3>SAP Corporate Profile Video</h3>
+                        <p>SAP Corporate Profile Video</p>
                     </div>
                     <div class="subtitle">
                         <p>Sharing our passion, realizing your ideas and visions.</p>
+                    </div>
+                    <div class="play">
+                        <img src="<?php echo $this->assetBaseurl; ?>play.png" alt="">
+                        <p>Play Video</p>
                     </div>
                 </div>
             </div>
@@ -70,21 +89,30 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-20">
+            <div class="col-md-60">
                 <div class="box-content">
-                    <div class="image-corporation"><img src="" alt=""></div>
+                    <div class="image-corporation">
+                        <img src="<?php echo $this->assetBaseurl; ?>logo-sap.png" alt="">
+                        <img src="<?php echo $this->assetBaseurl; ?>logo-amari.png" alt=""
+                        ><img src="<?php echo $this->assetBaseurl; ?>logo-multindo.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-md-20">
+                <div class="box-content">
+                    <div class="image-corporation"><img src="<?php echo $this->assetBaseurl; ?>logo-sap.png" alt=""></div>
                 </div>
             </div>
             <div class="col-md-20">
                 <div class="box-content">
-                    <div class="image-corporation"><img src="" alt=""></div>
+                    <div class="image-corporation"><img src="<?php echo $this->assetBaseurl; ?>logo-amari.png" alt=""></div>
                 </div>
             </div>
             <div class="col-md-20">
                 <div class="box-content">
-                    <div class="image-corporation"><img src="" alt=""></div>
+                    <div class="image-corporation"><img src="<?php echo $this->assetBaseurl; ?>logo-multindo.png" alt=""></div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
--- protected/views/layouts/_footer.php
+++ protected/views/layouts/_footer.php
@@ -1,54 +1,57 @@
 <section class="footer">
     <div class="prelative container">
         <div class="row">
-            <div class="col-md-25">
+            <div class="col-md-7">
                 <div class="box-content">
-                    <div class="image">
-                        <img src="<?php echo $this->assetBaseurl; ?>logo-footer.png" alt="">
-                        <p>Reich Architectural & Furniture Accessories</p>
+                    <div class="image"><img src="<?php echo $this->assetBaseurl; ?>logo-sap.png" alt=""></div>
+                </div>
+            </div>
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="help">
+                        <h5>WE’RE HERE TO HELP</h5>
+                        <p>Contact Us</p>
+                        <p>Faq</p>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="get-us">
+                        <h5>GET TO KNOW US</h5>
+                        <p>Company History</p>
+                        <p>Our Mission</p>
+                        <p>Quality Statement</p>
+                        <p>Locations</p>
+                        <p>Career</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="policies">
+                        <h5>POLICIES</h5>
+                        <p>SO 90001 Certification</p>
+                        <p>FSSC 22000 Certification</p>
+                        <p>Quality Policy</p>
+                    </div>
+                </div>
+            </div>
             <div class="col-md-8">
-                <div class="box-content menu">
-                    <p>Our Product Collections</p>
+                <div class="box-content">
+                    <div class="image-logo">
+                        <img src="<?php echo $this->assetBaseurl; ?>in-footer.png" alt="">
+                        <img src="<?php echo $this->assetBaseurl; ?>twitter-footer.png" alt=""></div>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="box-content menu">
-                    <p>About Us</p>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <div class="box-content menu">
-                    <p>Our Quality</p>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <div class="box-content menu">
-                    <p>Hot To Buy</p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="box-content menu">
-                    <p>Stores</p>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <div class="box-content menu">
-                    <p>Partnership</p>
-                </div>
-            </div>
-            <div class="col-md-30">
-                <div class="box-content wa">
-                    <p>Whatsapp Hotline & Chat</p>
-                    <img class="wa-footer" src="<?php echo $this->assetBaseurl; ?>wa-logo-footer.png" alt="">
-                    <p>081 5530 78875 (Click To Chat)</p>
+        <div class="garis-formhome"></div>
+        <div class="row">
+            <div class="col-md-60">
+                <div class="box-content-by">
+                    <p>Copyright © 2019 - Suryasukses Group. All rights reserved. Website design by <a href="https://www.markdesign.net/">Mark Design</a></p>
                 </div>
             </div>
         </div>
-        <div class="garis-formhome"></div>
     </div>
 </section>
