dec357504d5f280b3af5359f40191dad8772c061
ibnu fajar <ibnudrift@gmail.com>
2019-09-19 12:36:25 +0700

    change layouts foot

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -2,10 +2,8 @@
 <?php $this->beginContent('//layouts/main'); ?>
 
 <?php echo $this->renderPartial('//layouts/_header', array()); ?>
-<div class="clear"></div>
-<div class="yellows_headBottoms"></div>
-<div class="clear"></div>
+
 <?php echo $content ?>
 
-<?php echo $this->renderPartial('//layouts/_footer2', array()); ?>
+<?php echo $this->renderPartial('//layouts/_footer', array()); ?>
 <?php $this->endContent(); ?>
