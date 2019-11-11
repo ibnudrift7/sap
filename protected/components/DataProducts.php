74223a5a41f1aa8483fe2c3ea89d73bce0c747b3
ibnu fajar <ibnudrift@gmail.com>
2019-11-11 16:48:44 +0700

    small updt

---
+++ protected/components/DataProducts.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * sort order
+ * By Deory Pandu
+ */
+class SortOrder {
+
+    public static function sortButton($data, $names_id)
+    {
+        
+        $data = [
+
+                ];
+
+        return $data;        
+    }
+    
+}
